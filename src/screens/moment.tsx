a62db634859b8f01c3eed53cb217e46f3b258083
Mr WANG <2362999724@qq.com>
2024-07-17 18:30:13 +0800

    add loading for moment screen

--- src/screens/moment.tsx
+++ src/screens/moment.tsx
@@ -4,6 +4,7 @@ import {
   StatusBar,
   ScrollView,
   TouchableOpacity,
+  ActivityIndicator,
 } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import tw from 'twrnc'
@@ -16,8 +17,10 @@ export default function Moment({ navigation }) {
   const theme = useTheme()
 
   const [list, setlist] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
+    setLoading(true)
     getAllPublishedNotes()
       .then(data => {
         console.log('note data:::::', data)
@@ -26,48 +29,61 @@ export default function Moment({ navigation }) {
       .catch(err => {
         console.log('\x1b[31m%s\x1b[0m', 'get all note err:::', err)
       })
+      .finally(() => {
+        setLoading(false)
+      })
   }, [])
 
   return (
-    <View style={[{ backgroundColor: theme.colors.background }, tw`flex-1`]}>
+    <View
+      style={[
+        { backgroundColor: theme.colors.background },
+        tw`flex-1 justify-center`,
+      ]}
+    >
       <StatusBar hidden={true} />
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ padding: 4 }}
-      >
-        {list?.length > 0 ? (
-          list.map((item, index) => (
-            <View
-              style={tw`flex-row items-center justify-between mt-2`}
-              key={index}
-            >
-              {/* <UserCircleIcon size={30} color={'#aaa'} /> */}
-              <View>
-                <Text
-                  style={[
-                    {
-                      color: theme.colors.onBackground,
-                    },
-                    theme.fonts.titleMedium,
-                  ]}
-                >
-                  {item.title}
-                </Text>
-                <Text style={[{ color: theme.colors.secondary }]}>
-                  {transFBDate2Local(item.createTime)}
-                </Text>
-              </View>
-              <TouchableOpacity
-                onPress={() => navigation.navigate('DetailScreen')}
+
+      {loading ? (
+        <ActivityIndicator size="large" color={theme.colors.outline} />
+      ) : (
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ padding: 12 }}
+        >
+          {list?.length > 0 ? (
+            list.map((item, index) => (
+              <View
+                style={tw`flex-row items-center justify-between mt-2`}
+                key={index}
               >
-                <EyeIcon size={30} color={theme.colors.secondary} />
-              </TouchableOpacity>
-            </View>
-          ))
-        ) : (
-          <Text>Null data</Text>
-        )}
-      </ScrollView>
+                {/* <UserCircleIcon size={30} color={'#aaa'} /> */}
+                <View>
+                  <Text
+                    style={[
+                      {
+                        color: theme.colors.onBackground,
+                      },
+                      theme.fonts.titleMedium,
+                    ]}
+                  >
+                    {item.title}
+                  </Text>
+                  <Text style={[{ color: theme.colors.secondary }]}>
+                    {transFBDate2Local(item.createTime)}
+                  </Text>
+                </View>
+                <TouchableOpacity
+                  onPress={() => navigation.navigate('DetailScreen')}
+                >
+                  <EyeIcon size={30} color={theme.colors.secondary} />
+                </TouchableOpacity>
+              </View>
+            ))
+          ) : (
+            <Text>Null data</Text>
+          )}
+        </ScrollView>
+      )}
     </View>
   )
 }
