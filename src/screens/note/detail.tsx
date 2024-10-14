a7f29a519f5c5a3fa41deec6e1483ab53c2551d9
Mr WANG <2362999724@qq.com>
2024-10-14 20:38:37 +0800

    add animation fro note detail page

--- src/screens/note/detail.tsx
+++ src/screens/note/detail.tsx
@@ -1,19 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { RootStackParamList, ScreenProps, ScrennTypeEnum } from '@/types/screen'
 import { RouteProp, useRoute } from '@react-navigation/native'
 import { Note, getNote } from '@/service/articles'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Animated, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 import { WebView } from 'react-native-webview'
 import { RadioButton, useTheme } from 'react-native-paper'
 import { extractTextFromHTML } from '@/utils/utilsString'
 import Markdown from 'react-native-markdown-display'
 import { DocType } from './create'
 import tw from 'twrnc'
+import { CogIcon } from 'react-native-heroicons/outline'
 export default function NodeDetail({ navigation }: ScreenProps<ScrennTypeEnum.NodeDetail>) {
-  const { params } = useRoute<RouteProp<RootStackParamList>>()
   const [note, setNote] = useState<Note | null>(null)
   const [showType, setShowType] = useState<DocType>(null)
+  const slideAnimate = useRef(new Animated.Value(-50)).current
+
   const theme = useTheme()
+  const { params } = useRoute<RouteProp<RootStackParamList>>()
+
+  const openSlide = () => {
+    const toValue = slideAnimate.__getValue() === 0 ? -50 : 0
+    Animated.timing(slideAnimate, {
+      toValue: toValue,
+      useNativeDriver: true,
+      duration: 300,
+    }).start()
+
+    console.log('slideAnimate:::', slideAnimate)
+  }
 
   const fetchNote = (id: string) => {
     getNote(id).then(data => {
@@ -47,20 +61,25 @@ export default function NodeDetail({ navigation }: ScreenProps<ScrennTypeEnum.No
 `
 
   return (
-    <SafeAreaView style={styles.container}>
-      <RadioButton.Group onValueChange={value => setShowType(value)} value={showType}>
-        <View style={[tw`flex-row items-center px-2`]}>
-          <Text>Display DocType:</Text>
-          <View style={[tw`flex-row items-center`]}>
-            <RadioButton value="html" />
-            <Text>PLain</Text>
+    <SafeAreaView style={[styles.container, { backgroundColor: theme.colors.background }]}>
+      <Animated.View style={[tw`absolute z-10`, { transform: [{ translateY: slideAnimate }] }]}>
+        <RadioButton.Group onValueChange={value => setShowType(value)} value={showType}>
+          <View style={[tw`flex-row items-center px-2`]}>
+            <Text>Display DocType:</Text>
+            <View style={[tw`flex-row items-center`]}>
+              <RadioButton value="html" />
+              <Text>PLain</Text>
+            </View>
+            <View style={[tw`flex-row items-center`]}>
+              <RadioButton value="md" />
+              <Text>Markdown</Text>
+            </View>
           </View>
-          <View style={[tw`flex-row items-center`]}>
-            <RadioButton value="md" />
-            <Text>Markdown</Text>
-          </View>
-        </View>
-      </RadioButton.Group>
+        </RadioButton.Group>
+      </Animated.View>
+      <View style={[tw`flex-row justify-end p-2`, { backgroundColor: theme.colors.surface }]}>
+        <CogIcon size={22} color={theme.colors.onSurface} onPress={() => openSlide()} />
+      </View>
 
       {showType === 'html' && (
         <>
@@ -74,10 +93,10 @@ export default function NodeDetail({ navigation }: ScreenProps<ScrennTypeEnum.No
         </>
       )}
       {showType === 'md' && (
-        <View style={[tw`px-2`]}>
+        <ScrollView contentContainerStyle={{ padding: 6 }}>
           <Markdown>{note?.title}</Markdown>
           <Markdown>{note?.content}</Markdown>
-        </View>
+        </ScrollView>
       )}
     </SafeAreaView>
   )
